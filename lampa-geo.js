3b9217ad9a4e4180679c0dbbbad79d3f68bb93eb
cverianidze-arch <cverianidze@gmail.com>
2026-05-31 14:21:44 +0400

    Update lampa-geo.js

--- lampa-geo.js
+++ lampa-geo.js
@@ -5,47 +5,41 @@
         var movieObject = component.activity.data; 
 
         this.search = function () {
-            // ვიღებთ ფილმის სახელს ლამპას ბაზიდან
             var title = movieObject.original_title || movieObject.title;
             var year = new Date(movieObject.release_date || movieObject.first_air_date).getFullYear();
 
-            // მოთხოვნა იგზავნება MyKadri-ს ძებნის API-ზე
-            // (რეალურ პირობებში შეიძლება დაგჭირდეს პროქსის მიწერა URL-ის წინ)
-            var searchUrl = 'https://api.mykadri.tv/v1/search?query=' + encodeURIComponent(title);
+            // 1. პირველ ეტაპზე, მოდი იძულებით მივაწოდოთ ლამპას ერთი მუშა სტრიმი,
+            // რათა დავინახოთ, რომ საერთოდ ჩნდება სიაში მაიკადრის ღილაკი.
+            var testStream = {
+                title: 'MyKadri (ტესტი ჩაიტვირთა)',
+                url: 'https://demo.unified-streaming.com/kaltura/the-secret-life-of-pets.isml/.m3u8', // სატესტო მუშა ვიდეო
+                quality: '1080p',
+                language: 'GEO'
+            };
+
+            // იძულებით ვახატავინებთ ღილაკს
+            component.addStream([testStream]);
+
+            // 2. პარალელურად ვუშვებთ რეალურ ძებნას მაიკადრიზე პროქსის გავლით
+            // (გამოიყენება სატესტო პროქსი corsproxy.io)
+            var proxy = "https://corsproxy.io/?";
+            var searchUrl = proxy + encodeURIComponent('https://api.mykadri.tv/v1/search?query=' + title);
 
             $.ajax({
                 url: searchUrl,
                 method: 'GET',
                 dataType: 'json',
                 success: function (response) {
-                    // response არის ის, რასაც საიტი გვიბრუნებს.
-                    // ვუშვებთ, რომ საიტი აბრუნებს მასივს (შედეგებს)
                     if (response && response.length > 0) {
-                        
-                        // ვეძებთ პირველ ფილმს, რომელიც წლით ემთხვევა
-                        var foundMovie = response.find(function(item) {
-                            return item.year == year;
-                        }) || response[0]; // თუ წლით ვერ იპოვა, ავიღოთ პირველივე
-
-                        // როცა ფილმის ID გვაქვს, MyKadri-ს წესით სჭირდება ვიდეო ფაილის ამოღება.
-                        // ვთქვათ, ვიდეოს პირდაპირი ლინკი ასე იწყობა მათი სერვერიდან:
-                        var videoUrl = 'https://node.mykadri.tv/hls/' + foundMovie.id + '/master.m3u8';
-
-                        var streams = [
-                            {
-                                title: 'MyKadri (ქართულად)',
-                                url: videoUrl,
-                                quality: '1080p',
-                                language: 'GEO'
-                            }
-                        ];
-
-                        // აი ამ მომენტში ლამპა დაინახავს რეალურ ობიექტს და გამოაჩენს ონლაინ ღილაკს!
-                        component.addStream(streams);
+                        // თუ რეალურად იპოვა ფილმი, დაამატებს მეორე, ნამდვილ ღილაკსაც
+                        var realStream = {
+                            title: 'MyKadri: ' + response[0].title,
+                            url: 'https://node.mykadri.tv/hls/' + response[0].id + '/master.m3u8',
+                            quality: '720p',
+                            language: 'GEO'
+                        };
+                        component.addStream([realStream]);
                     }
-                },
-                error: function() {
-                    console.log('MyKadri-ზე ფილმი ვერ მოიძებნა.');
                 }
             });
         };
