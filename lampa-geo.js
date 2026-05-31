41a1fd3ea0af5130000a26eb98d182d56de6fe7f
cverianidze-arch <cverianidze@gmail.com>
2026-05-31 15:07:40 +0400

    Update lampa-geo.js

--- lampa-geo.js
+++ lampa-geo.js
@@ -1,7 +1,8 @@
 (function () {
     'use strict';
 
-    function showGeorgianSources(movieData) {
+    // ფუნქცია, რომელიც რთავს ვიდეოს
+    function playGeoMovie(movieData) {
         var title = movieData.original_title || movieData.title;
         var year = new Date(movieData.release_date || movieData.first_air_date).getFullYear();
 
@@ -10,7 +11,7 @@
             items: [
                 {
                     title: 'MyKadri (ტესტირება)',
-                    subtitle: year + ' • 1080p • ქართული დუბლიაჟი',
+                    subtitle: year + ' • 1080p • GEO',
                     url: 'https://demo.unified-streaming.com/kaltura/the-secret-life-of-pets.isml/.m3u8'
                 }
             ],
@@ -27,28 +28,31 @@
         });
     }
 
+    // პლაგინის გაშვება
     if (window.Lampa) {
         Lampa.Plugins.add('georgian_sources', {
             title: 'Georgian Streaming',
             description: 'ფილმები ქართულად',
             auth: false,
             start: function () {
+                // ტესტირებისთვის: ლამპას ჩართვისას ეგრევე ამოაგდებს შეტყობინებას
+                setTimeout(function() {
+                    Lampa.Noty.show('ქართული პლაგინი წარმატებით ჩაიტვირთა!');
+                }, 2000);
+
+                // ვამატებთ ახალ ჩანართს (Tab) ფილმის შიდა მენიუში
                 Lampa.Events.on('activity', function (event) {
                     if (event.component === 'full' && event.type === 'start') {
                         var activity = event.target;
                         
-                        var geoButton = $('<div class="full-start__button selector button--geo-sources" style="background: #00b060; color: #fff; font-weight: bold; padding: 10px 15px; border-radius: 6px; margin-left: 10px; display: inline-flex; align-items: center; cursor: pointer;"><span style="font-size: 14px;">GEO</span></div>');
-                        
-                        geoButton.on('hover:enter', function () {
-                            showGeorgianSources(activity.data);
-                        });
-
-                        setTimeout(function() {
-                            var buttonsContainer = activity.dom.find('.full-start__buttons');
-                            if (buttonsContainer.length > 0 && activity.dom.find('.button--geo-sources').length === 0) {
-                                buttonsContainer.append(geoButton);
+                        // ვამატებთ ახალ პუნქტს მენიუში (მსახიობების და მსგავსი ფილმების გვერდით)
+                        activity.page.items.push({
+                            title: 'ყურება ქართულად',
+                            id: 'geo_online',
+                            onSelect: function() {
+                                playGeoMovie(activity.data);
                             }
-                        }, 500);
+                        });
                     }
                 });
             }
