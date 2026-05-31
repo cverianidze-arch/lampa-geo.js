f73313b40b9faeb8ace20eee76fd786bc4e5fd2e
cverianidze-arch <cverianidze@gmail.com>
2026-05-31 14:49:08 +0400

    Update lampa-geo.js

--- lampa-geo.js
+++ lampa-geo.js
@@ -1,76 +1,63 @@
 (function () {
     'use strict';
 
-    // ვქმნით ფუნქციას, რომელიც მოძებნის ფილმს
-    function GeoBalancer(object) {
-        var network = new Lampa.Reguest();
-        var scroll  = new Lampa.Scroll({mask:true,over:true});
-        var files   = new Lampa.Files(object);
-        var filter  = new Lampa.Filter(object);
-        
-        var title = object.original_title || object.title;
-        var year = new Date(object.release_date || object.first_air_date).getFullYear();
+    // ფუნქცია, რომელიც აჩვენებს ქართულ წყაროებს
+    function showGeorgianSources(movieData) {
+        var title = movieData.original_title || movieData.title;
+        var year = new Date(movieData.release_date || movieData.first_air_date).getFullYear();
 
-        // ეს ფუნქცია აკონტროლებს, რა უნდა მოხდეს როცა ამ ბალანსერს ავირჩევთ
-        this.start = function () {
-            this.activity.loader(true);
-
-            // სატესტო სტრიმი, რომელსაც პირდაპირ ვაწვდით ლამპას
-            var testFolder = {
-                title: 'MyKadri (ტესტი)',
-                subtitle: year + ' • GEO • 1080p',
-                quality: '1080p',
-                url: 'https://demo.unified-streaming.com/kaltura/the-secret-life-of-pets.isml/.m3u8',
-                timeline: object.timeline
-            };
-
-            // ვქმნით სიას მომხმარებლისთვის
-            var items = [testFolder];
-
-            // ვთიშავთ ლოადერს და ვხატავთ ფაილს
-            this.activity.loader(false);
-            
-            files.append({
-                title: 'ფილმები ქართულად',
-                items: items,
-                onSelect: function(item) {
-                    Lampa.Player.play({
-                        url: item.url,
-                        title: title
-                    });
-                    Lampa.Player.playlist([item]);
+        // ვქმნით მენიუს, რომელიც ეკრანზე ამოვარდება
+        Lampa.Select.show({
+            title: 'ქართული სერვერები',
+            items: [
+                {
+                    title: 'MyKadri (ტესტირება)',
+                    subtitle: year + ' • 1080p • ქართული დუბლიაჟი',
+                    url: 'https://demo.unified-streaming.com/kaltura/the-secret-life-of-pets.isml/.m3u8' // სატესტო ვიდეო
                 }
-            });
-
-            this.activity.toggle();
-        };
-
-        this.create = function () {
-            return scroll.render();
-        };
-
-        this.destroy = function () {
-            scroll.destroy();
-            files.destroy();
-        };
+            ],
+            onSelect: function (item) {
+                // როცა მომხმარებელი აირჩევს ფილმს, ირთვება პლეერი
+                Lampa.Player.play({
+                    url: item.url,
+                    title: title
+                });
+                Lampa.Player.playlist([item]);
+            },
+            onBack: function () {
+                Lampa.Controller.toggle('full_start');
+            }
+        });
     }
 
-    // ეს არის მთავარი ხაზი, რომელიც ლამპას ეუბნება: "დაამატე ახალი წყარო წყაროების სიაში"
+    // პლაგინის ინტეგრაცია ლამპას ინტერფეისში
     if (window.Lampa) {
-        // ვარეგისტრირებთ ახალ კომპონენტს წყაროებისთვის
-        Lampa.Component.add('geo_movies', GeoBalancer);
-
         Lampa.Plugins.add('georgian_sources', {
             title: 'Georgian Streaming',
-            description: 'ქართული საიტების ძებნა',
+            description: 'ფილმები ქართულად',
             auth: false,
             start: function () {
-                // ვამატებთ ღილაკს "Смотреть"-ის დაჭერის შემდგომ მენიუში
-                Lampa.Events.on('online', function (event) {
-                    event.component.add({
-                        title: 'MyKadri', // აი ეს სახელი გამოჩნდება სიაში!
-                        component: 'geo_movies'
-                    });
+                // ვუსმენთ ფილმის აღწერის გვერდის ჩატვირთვას
+                Lampa.Events.on('activity', function (event) {
+                    if (event.component === 'full' && event.type === 'start') {
+                        var activity = event.target;
+                        
+                        // ვქმნით პატარა მწვანე ღილაკს, რომელიც ჩაჯდება სანიშნეების (Bookmark) გვერდით
+                        var geoButton = $('<div class="full-start__button selector button--geo-sources" style="background: #00b060; color: #fff; font-weight: bold; padding: 10px 15px; border-radius: 6px; margin-left: 10px; display: inline-flex; align-items: center; cursor: pointer;"><span style="font-size: 14px;">GEO</span></div>');
+                        
+                        // როცა ღილაკს დავაჭერთ
+                        geoButton.on('hover:enter', function () {
+                            showGeorgianSources(activity.data);
+                        });
+
+                        // ველოდებით სანამ ინტერფეისი სრულად აიგება და ჩავამატებთ ღილაკს
+                        setTimeout(function() {
+                            var buttonsContainer = activity.dom.find('.full-start__buttons');
+                            if (buttonsContainer.length > 0 && activity.dom.find('.button--geo-sources').length === 0) {
+                                buttonsContainer.append(geoButton);
+                            }
+                        }, 500);
+                    }
                 });
             }
         });
