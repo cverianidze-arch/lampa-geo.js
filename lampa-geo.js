c98c96c764cd23fe4ba3fc5b2061bb393157d633
cverianidze-arch <cverianidze@gmail.com>
2026-05-31 14:16:05 +0400

    Add files via upload

--- lampa-geo.js
+++ lampa-geo.js
@@ -5,56 +5,56 @@
         var movieObject = component.activity.data; 
 
         this.search = function () {
-            // ვიღებთ ორიგინალ სახელს და წელს ლამპას ბაზიდან
+            // ვიღებთ ფილმის სახელს ლამპას ბაზიდან
             var title = movieObject.original_title || movieObject.title;
             var year = new Date(movieObject.release_date || movieObject.first_air_date).getFullYear();
 
-            // საიტების API მისამართები (მაგალითისთვის Mykadri და Ge.movie)
-            // შენიშვნა: CORS ბლოკირების ასარიდებლად ბრაუზერში, რეალურ პლაგინს სჭირდება პროქსი (მაგ. cors-anywhere)
-            var sources = [
-                {
-                    name: 'MyKadri',
-                    searchUrl: 'https://api.mykadri.tv/v1/search?query=' + encodeURIComponent(title)
-                },
-                {
-                    name: 'GeMovie',
-                    searchUrl: 'https://ge.movie/api/search?q=' + encodeURIComponent(title)
-                }
-            ];
+            // მოთხოვნა იგზავნება MyKadri-ს ძებნის API-ზე
+            // (რეალურ პირობებში შეიძლება დაგჭირდეს პროქსის მიწერა URL-ის წინ)
+            var searchUrl = 'https://api.mykadri.tv/v1/search?query=' + encodeURIComponent(title);
 
-            // სათითაოდ ვამოწმებთ საიტებს
-            sources.forEach(function(source) {
-                $.ajax({
-                    url: source.searchUrl,
-                    method: 'GET',
-                    success: function (response) {
-                        // აქ იწერება კონკრეტული საიტის პასუხის დამუშავება
-                        // მაგალითად, თუ საიტმა დააბრუნა ვიდეოს ID და ლინკი:
+            $.ajax({
+                url: searchUrl,
+                method: 'GET',
+                dataType: 'json',
+                success: function (response) {
+                    // response არის ის, რასაც საიტი გვიბრუნებს.
+                    // ვუშვებთ, რომ საიტი აბრუნებს მასივს (შედეგებს)
+                    if (response && response.length > 0) {
                         
-                        var resultStream = {
-                            title: source.name + ' (' + year + ')',
-                            url: 'https://სერვერის-მისამართი/putput.m3u8', // აქ ჩაჯდება დინამიური .m3u8 ლინკი
-                            quality: '1080p',
-                            language: 'GEO'
-                        };
+                        // ვეძებთ პირველ ფილმს, რომელიც წლით ემთხვევა
+                        var foundMovie = response.find(function(item) {
+                            return item.year == year;
+                        }) || response[0]; // თუ წლით ვერ იპოვა, ავიღოთ პირველივე
 
-                        // ვამატებთ სტრიმს ლამპას სიაში
-                        var streams = [resultStream];
+                        // როცა ფილმის ID გვაქვს, MyKadri-ს წესით სჭირდება ვიდეო ფაილის ამოღება.
+                        // ვთქვათ, ვიდეოს პირდაპირი ლინკი ასე იწყობა მათი სერვერიდან:
+                        var videoUrl = 'https://node.mykadri.tv/hls/' + foundMovie.id + '/master.m3u8';
+
+                        var streams = [
+                            {
+                                title: 'MyKadri (ქართულად)',
+                                url: videoUrl,
+                                quality: '1080p',
+                                language: 'GEO'
+                            }
+                        ];
+
+                        // აი ამ მომენტში ლამპა დაინახავს რეალურ ობიექტს და გამოაჩენს ონლაინ ღილაკს!
                         component.addStream(streams);
-                    },
-                    error: function() {
-                        console.log(source.name + '-ზე ფილმი ვერ მოიძებნა ან სერვერი მიუწვდომელია.');
                     }
-                });
+                },
+                error: function() {
+                    console.log('MyKadri-ზე ფილმი ვერ მოიძებნა.');
+                }
             });
         };
     }
 
-    // პლაგინის რეგისტრაცია ლამპას სისტემაში
     if (window.Lampa) {
         Lampa.Plugins.add('georgian_sources', {
             title: 'Georgian Streaming',
-            description: 'ქართული საიტების ძებნა (Mykadri, Geosaitebi, GeMovie, Kinoflix)',
+            description: 'ქართული საიტების ძებნა',
             auth: false,
             start: function () {
                 Lampa.Events.on('view_sources', function (event) {
