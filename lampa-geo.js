ca72b410d9cc2dfd3451cd9758942ec6f2601675
cverianidze-arch <cverianidze@gmail.com>
2026-05-31 14:36:33 +0400

    Update lampa-geo.js

--- lampa-geo.js
+++ lampa-geo.js
@@ -1,60 +1,76 @@
 (function () {
     'use strict';
 
-    // ძირითადი ფუნქცია, რომელიც გახსნის ქართულ ფლეერს
-    function startGeoOnline(object) {
+    // ვქმნით ფუნქციას, რომელიც მოძებნის ფილმს
+    function GeoBalancer(object) {
+        var network = new Lampa.Reguest();
+        var scroll  = new Lampa.Scroll({mask:true,over:true});
+        var files   = new Lampa.Files(object);
+        var filter  = new Lampa.Filter(object);
+        
         var title = object.original_title || object.title;
-        
-        // ვქმნით ლამპას სტანდარტულ პოპაპ/მენიუს ფანჯარას
-        var modal = $('<div><div class="broadcast__text" style="text-align:center; padding: 20px;">იტვირთება ქართული წყაროები...</div></div>');
-        
-        Lampa.Select.show({
-            title: 'ქართული წყაროები',
-            items: [
-                {
-                    title: 'MyKadri (ტესტი ჩაიტვირთა)',
-                    subtitle: '1080p • GEO',
-                    url: 'https://demo.unified-streaming.com/kaltura/the-secret-life-of-pets.isml/.m3u8'
+        var year = new Date(object.release_date || object.first_air_date).getFullYear();
+
+        // ეს ფუნქცია აკონტროლებს, რა უნდა მოხდეს როცა ამ ბალანსერს ავირჩევთ
+        this.start = function () {
+            this.activity.loader(true);
+
+            // სატესტო სტრიმი, რომელსაც პირდაპირ ვაწვდით ლამპას
+            var testFolder = {
+                title: 'MyKadri (ტესტი)',
+                subtitle: year + ' • GEO • 1080p',
+                quality: '1080p',
+                url: 'https://demo.unified-streaming.com/kaltura/the-secret-life-of-pets.isml/.m3u8',
+                timeline: object.timeline
+            };
+
+            // ვქმნით სიას მომხმარებლისთვის
+            var items = [testFolder];
+
+            // ვთიშავთ ლოადერს და ვხატავთ ფაილს
+            this.activity.loader(false);
+            
+            files.append({
+                title: 'ფილმები ქართულად',
+                items: items,
+                onSelect: function(item) {
+                    Lampa.Player.play({
+                        url: item.url,
+                        title: title
+                    });
+                    Lampa.Player.playlist([item]);
                 }
-            ],
-            onSelect: function (item) {
-                // როცა მომხმარებელი დააწვება ფილმს, ირთვება ლამპას პლეერი
-                Lampa.Player.play({
-                    url: item.url,
-                    title: title
-                });
-                
-                // ვნიშნავთ პლეერს, რომ მუშაობს
-                Lampa.Player.playlist([item]);
-            },
-            onBack: function () {
-                Lampa.Controller.toggle('full_start');
-            }
-        });
+            });
+
+            this.activity.toggle();
+        };
+
+        this.create = function () {
+            return scroll.render();
+        };
+
+        this.destroy = function () {
+            scroll.destroy();
+            files.destroy();
+        };
     }
 
-    // ვარეგისტრირებთ პლაგინს ლამპას სისტემაში
+    // ეს არის მთავარი ხაზი, რომელიც ლამპას ეუბნება: "დაამატე ახალი წყარო წყაროების სიაში"
     if (window.Lampa) {
+        // ვარეგისტრირებთ ახალ კომპონენტს წყაროებისთვის
+        Lampa.Component.add('geo_movies', GeoBalancer);
+
         Lampa.Plugins.add('georgian_sources', {
             title: 'Georgian Streaming',
             description: 'ქართული საიტების ძებნა',
             auth: false,
             start: function () {
-                // ვუსმენთ ფილმის გვერდის გახსნას ინტერფეისში
-                Lampa.Events.on('activity', function (event) {
-                    if (event.component === 'full' && event.type === 'start') {
-                        var activity = event.target;
-                        
-                        // ვამატებთ ახალ ღილაკს ფილმის მენიუში, ბალანსერების გვერდით
-                        var button = $('<div class="full-start__button selector button--geo" style="background: #24b47e; color: #fff;"><svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right: 8px; vertical-align: middle;"><path d="M8 5V19L19 12L8 5Z" fill="currentColor"/></svg><span style="vertical-align: middle;">ყურება ქართულად</span></div>');
-                        
-                        button.on('hover:enter', function () {
-                            startGeoOnline(activity.data);
-                        });
-
-                        // ვსვამთ ღილაკს ლამპას სტანდარტული ღილაკების პანელში
-                        activity.dom.find('.full-start__buttons').append(button);
-                    }
+                // ვამატებთ ღილაკს "Смотреть"-ის დაჭერის შემდგომ მენიუში
+                Lampa.Events.on('online', function (event) {
+                    event.component.add({
+                        title: 'MyKadri', // აი ეს სახელი გამოჩნდება სიაში!
+                        component: 'geo_movies'
+                    });
                 });
             }
         });
